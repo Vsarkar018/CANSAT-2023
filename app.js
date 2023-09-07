9ce8b7cdd3d4fb7930fd61a1391f2bd19a87c46e
Vsarkar018 <sarkarvishal018@gmail.com>
2023-09-08 03:39:28 +0530

    CANSAT GCS

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+app.use(require("cors")());
+const { SerialPort, DelimiterParser } = require("serialport");
+const { XBeeAPI } = require("xbee-api");
+const serialPort = new SerialPort({ path: "COM3", baudRate: 9600 });
+const xbee = new XBeeAPI({ api_mode: 2 });
+
+const parser = serialPort.pipe(new DelimiterParser({ delimiter: "~" }));
+
+const port = process.env.PORT || 5000;
+const server = app.listen(
+  port,
+  console.log(`Server listening to the port ${port}`)
+);
+
+const io = socketio(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+serialPort.on("open", () => {
+  console.log("Serial Port Open");
+});
+
+parser.on("data", (telemetry) => {
+  const frame = xbee.parseFrame(telemetry);
+  const tele = frame.data.toString("utf8").trim();
+  // io.emit("telemetry", tele);
+  console.log(tele);
+});
